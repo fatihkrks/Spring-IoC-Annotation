d864a965aa6d9913cc7bbe7151e46e10cf9cd246
Fatih Karakas <fatihkrks95@gmail.com>
2020-04-14 08:50:49 +0300

    springIoCAnnotation

---
+++ src/com/springdemo/MySqlCustomerDal.java
@@ -0,0 +1,19 @@
+package com.springdemo;
+
+public class MySqlCustomerDal implements ICustomerDal{
+	String connectionString;
+	public String getConnectionString() {
+		return connectionString;
+	}
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+	@Override
+	public void add() {
+		System.out.println("Connection String : " +this.connectionString);
+		System.out.println("Mysql veritabanına eklendi");
+		
+	}
+	
+
+}
